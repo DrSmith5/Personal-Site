d1c59ac89df61825d4e976bb636352de3686a9fa
Devin <104108633+DrSmith5@users.noreply.github.com>
2024-04-28 16:17:17 -0700

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+function loadProjectPage(){
+    window.location.href = "http://127.0.0.1:5500/index2.html";
+}
+
+function loadGithubPage(){
+    window.open("https://github.com/DrSmith5");
+}
+
+function loadLinkedinPage(){
+    window.open("https://www.linkedin.com/in/devin-r-smith");
+}
